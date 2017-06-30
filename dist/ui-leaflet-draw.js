eddf8a25e3ff2ab41694612d74abccde1c0e3e09
Eugene Huang <eugenehuang.sg@gmail.com>
2017-06-30 20:29:22 +0800

    Build dist for ng-strict-di changes

--- dist/ui-leaflet-draw.js
+++ dist/ui-leaflet-draw.js
@@ -3,7 +3,7 @@
  *
  * @version: 0.0.5
  * @author: Nicholas McCready
- * @date: Sat Mar 18 2017 16:04:33 GMT-0400 (EDT)
+ * @date: Fri Jun 30 2017 20:28:47 GMT+0800 (+08)
  * @license: MIT
  */
 
@@ -68,15 +68,17 @@
         scope: false,
         replace: false,
         require: ['leaflet'],
-        controller: function($scope) {
-          this._deferredDrawTool = $q.defer();
-          this.getDrawTool = function() {
-            return this._deferredDrawTool.promise;
-          };
-          return this.getScope = function() {
-            return $scope;
-          };
-        },
+        controller: [
+          '$scope', function($scope) {
+            this._deferredDrawTool = $q.defer();
+            this.getDrawTool = function() {
+              return this._deferredDrawTool.promise;
+            };
+            return this.getScope = function() {
+              return $scope;
+            };
+          }
+        ],
         link: function(scope, element, attrs, controller) {
           var _deferred, _featureGroup, _optionsEditedInDirective, leafletScope, mapController;
           mapController = controller[0];
